031b1ab7428fed8bb85075a51ebdaac1e9f8cec0
Nix-code <nishantbanjade07@gmail.com>
2023-03-10 11:44:36 +0530

    api is getting called using axios and useEffect

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,5 +1,6 @@
 // next step after header is the product listing
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 
@@ -7,8 +8,31 @@ const ProductListing = () => {
     // we have to use useSelector in order to get the access
     // from the redux store and so that we can fetch the data we
     // need for our work
+
+ 
+    
     const products = useSelector((state) => state.allProducts.products);
-    console.log(products);
+
+
+       // getting the contents from the api
+       const fetchProducts = async () => {
+        const response = await axios
+          .get("https://fakestoreapi.com/products")
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            console.log("Err", err);
+          });
+        console.log(response);
+    };
+    useEffect(()=> {
+        fetchProducts();
+    },[]);
+
+
+    
+    // console.log(products);
     return (
         // returning jSX 
         
