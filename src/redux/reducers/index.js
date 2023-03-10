4e7a196ec121f5ddbd50558cd9d53f5ef7817d1d
Nix-code <nishantbanjade07@gmail.com>
2023-03-10 09:52:06 +0530

    redux dev tool can read store now. Previously, the combineReducer code was commented , reducers/index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,12 @@
 // sometimes we may have many reducer functions together
 // we need to combine all those reducers in a index.js
 // combineReducer is inside redux
-// import { combineReducers } from "redux";
-// import { productReducer } from './productReducer';
-// // const reducers = combineReducers({});
-// const reducers = combineReducers({
-//     // key: val
-//     allProducts: productReducer,
-// }) ;
-// // // created action, action-types, reducer, combined reducers , time to create store
-// export default reducers;
+import { combineReducers } from "redux";
+import { productReducer } from './productReducer';
+// const reducers = combineReducers({});
+const reducers = combineReducers({
+    // key: val
+    allProducts: productReducer,
+}) ;
+// // created action, action-types, reducer, combined reducers , time to create store
+export default reducers;
